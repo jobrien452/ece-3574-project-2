9c7f48eaa71d719a0ee87e943034878ec13bb9f0
Jake O'Brien <jakeo8@vt.edu>
2015-10-20 17:52:32 -0400

    finished line test update cmakelists for all testing executables

--- lineTest.cpp
+++ lineTest.cpp
@@ -5,7 +5,6 @@
 #include <QPainter>
 
 #include "line.h"
-#include "drawing.h"
 
 class LineTest: public QOjbect
 {
@@ -23,51 +22,79 @@ class LineTest: public QOjbect
 
     private:
 	Line * l;
-	bool x;
-	QPoint p;
-	QPixmap b;
+	QPoint p1,p2;
+	QPixmap b1, b2;
 	QPainter paint;
 };
 
 void LineTest::initTestCase()
-{
+{   
     l = new Line();
-    x = false;
-    p = QPoint(100,100);
-    b = QPixmap(200,200);
-    paint = paint(&b);
-
+    p1 = QPoint(100,100);
+    p2 = QPoint(1,1);
+    b1 = QPixmap(200,200);
+    b2 = QPixmap(200,200);
+    paint = paint(b1);
 }
 
 void LineTest::testSetPressed()
-{
-    
+{  
+   l -> setPressed(true, p1);
+   l -> onMove(p2, true);
+   l -> setPressed(false,p1);
+   l -> setPressed(true, p1);
+   l -> onMove(p2, true);
+   l -> se9tPressed(false,p1);
+   QVERIFY(l->onSnap(p1) == 0);
+   QVERIFY(l->onSnap(p2) == 1);
 }
 
 void LineTest::testGetSnap()
 {
+   QVERIFY(l->getSnap(p1) == "Line's Start");
+   QVERIFY(l->getSnap(p2) == "Line's End");
+   QVERIFY(l->getSnap(QPoint(50,50)) == "");
 }
 
 void LineTest::testAbort()
 {
+   l -> setPressed(true, QPoint(50,50));
+   l -> abort();
+   QVERIFY(l->getSnap(QPoint(50,50)) == "");
 }
 
 void LineTest::testGetCenter()
 {
-
-
+   //unused function 
+   QVERIFY(l->getCenter() == QPoint(0,0));
 }
 
 void LineTest::testRender()
 {
+   l -> setPressed(true, p1);
+   l -> onMove(QPoint(50,50), true);
+   l -> trigRen(paint, b1);
+   l -> setPressed(false,p1);
+   b2 = l -> trigRen(paint, b1);
+   QVERIFY(b1.cacheKey() == b2.cacheKey()); 
 }
 
 void LineTest::testOnMoveRen()
 {
+   b1 = QPixmap(200,200);
+   b2 = QPixmap(200,200);
+   QPainter b2painter(&b2);
+   b2painter.drawLine(QLine(QPoint(0,0),QPoint(10,10)));
+   l->setPressed(true,QPoint(0,0));
+   l->onMove(QPoint(10,10), true);
+   l->trigRen(paint, b1);
+   QVERIFY(b1.cacheKey() == b2.cacheKey());
+   
 }
 
 void LineTest::cleanupTestCase()
 {
+   //do nothing
 }
 
 QTEST_MAIN(LineTest)
